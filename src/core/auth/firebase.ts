d07da15cdc186ae48306350c478912bc7e024d04
folioportfolio <nici@hotmail.cz>
2026-01-18 01:54:15 +0100

    Removed secrets

--- src/core/auth/firebase.ts
+++ src/core/auth/firebase.ts
@@ -1,18 +1,10 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyBVmvbeZZaK_wXRaAn5Qg_B0kDvxzMMY6c",
-    authDomain: "render-status-app.firebaseapp.com",
-    projectId: "render-status-app",
-    storageBucket: "render-status-app.firebasestorage.app",
-    messagingSenderId: "816068660214",
-    appId: "1:816068660214:web:f7c295f6e301e6576095d7"
-};
-
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = initializeApp({
+    projectId: import.meta.env.VITE_FB_PROJECT_ID,
+    apiKey: import.meta.env.VITE_FB_API_KEY,
+    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+});
 export const auth = getAuth(app);
